9d582aaeeb876435e00ffff1fe7808f159d8f445
OverMg <alejitasoto111@gmail.com>
2022-09-12 20:21:57 -0500

    links

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { MoviesGrid } from "./MoviesGrid";
+import { MoviesGrid } from "./components/MoviesGrid";
+import styles from "./App.module.css";
+import { BrowserRouter as Router, Routes, Route as Route, Link } from "react-router-dom";
+import { MovieDetails } from "./pages/MovieDetails";
+import { LandingPage } from "./pages/LandingPage";
 
 export function App() {
-    return <div>
-        <header>
-            <h1>PELISPLUS</h1>
-        </header>
-        <main>
-            <MoviesGrid/>
-        </main>
-    </div>
+  return (
+    <Router>
+      <header>
+        <h1 className={styles.title}>Movies</h1>
+        <Link to="/">Home</Link>
+        <br/>
+        <Link to="/movie">Movie</Link>
+      </header>
+      <main>
+        <Routes>
+            <Route exact path="/">Homee</Route>
+            <Route path="/movie">homeee</Route>
+        </Routes>
+      </main>
+    </Router>
+  );
 }
+
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,8 @@ export function MovieCard({ movie }) {
   const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
   return (
     <li className={styles.MovieCard}>
-      <img
+      <img width={230}
+      height={345}
       className={styles.movieImage} 
       src={imageUrl}
       alt={movie.title} />
