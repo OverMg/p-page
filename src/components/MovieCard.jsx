bead7d25dd82de2a885be77c81792579228241c1
OverMg <alejitasoto111@gmail.com>
2022-09-13 10:26:27 -0500

    found

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ export function MovieCard({ movie }) {
   const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
   return (
     <li className={styles.MovieCard}>
-      <Link to={"/movies/" + movie.id}>
+      <Link to={"/movie/" + movie.id}>
         <img
           width={230}
           height={345}
